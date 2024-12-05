f1e737a02cd149fe35703aa2fd7479bf1445da83
Shadab Shah <85732713+shadabshah1680@users.noreply.github.com>
2024-12-05 07:50:09 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,4 @@
-module "vpc" {
-  source          = "./modules/vpc"
-  name            = var.vpc.name
-  cidr            = var.vpc.cidr
-  azs             = var.vpc.azs
-  private_subnets = var.vpc.private_subnets
-  private_subnet_names = [
-    for i, subnet in var.vpc.private_subnet_names : "${subnet}-private-${element(var.vpc.azs, i % length(var.vpc.azs))}"
-  ]
-  public_subnets = var.vpc.public_subnets
-  public_subnet_names = [
-    for i, subnet in var.vpc.public_subnet_names : "${subnet}-public-${element(var.vpc.azs, i % length(var.vpc.azs))}"
-  ]
-  enable_nat_gateway     = var.vpc.enable_nat_gateway
-  single_nat_gateway     = var.vpc.single_nat_gateway
-  one_nat_gateway_per_az = var.vpc.one_nat_gateway_per_az
-  tags = {
-    Terraform   = "true"
-    Environment = var.environment
-    Project     = var.project
-  }
-}
+
 
 module "eks_cluster" {
   source             = "./modules/eks-cluster"
@@ -41,7 +20,7 @@ module "eks_node_groups" {
   for_each        = var.eks_managed_node_groups
   cluster_name    = each.value.cluster_name
   node_group_name = each.value.node_group_name
-  subnet_ids      = module.vpc.private_subnets
+  subnet_ids      = var.private-subnet-ids
   environment     = var.environment
   desired_size    = each.value.node_group_desired_size
   min_size        = each.value.node_group_min_size
